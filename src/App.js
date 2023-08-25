b1f4510a4a6744400f40ef2bcd28419ce8e0e4ae
Nileshp4321 <86780342+Nileshp4321@users.noreply.github.com>
2023-08-25 11:14:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Search from './Components/Seacrh';
+
+function App() {
+  return (
+    <>
+        <NavBar/>
+        <Search/>
+    </>
+  );
+}
+
+export default App;
